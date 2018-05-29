a6fbd4314591de6c2ab38e74df1a2bfd5abaf59a
Beligh Hamdi <beligh.hamdi@gmail.com>
2018-05-29 12:41:38 +0100

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import {SharedModule} from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/books/books-page/books-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cyn-books-page',
+  templateUrl: './books-page.component.html',
+  styleUrls: ['./books-page.component.css']
+})
+export class BooksPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
