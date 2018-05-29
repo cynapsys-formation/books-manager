fd226b20a2bb4354bb3370c7895876992e1b1362
Juggy001 <fakhreddine.kridane@esprit.tn>
2018-05-29 12:51:37 +0100

    books

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', loadChildren:'./pages/pages.module#PagesModule'},
+{path: 'books', loadChildren: './books/books.module#BooksModule'} ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageComponent } from './components/message/message.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MessageComponent],
+  declarations: [MessageComponent, NotificationComponent],
   exports: [MessageComponent]
 })
 export class SharedModule { }
